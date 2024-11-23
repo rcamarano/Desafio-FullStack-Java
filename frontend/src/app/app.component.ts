a63d3c09182550a1eda1e74373a27aabe2296084
rcamarano <rogeriocamarano@gmail.com>
2024-11-23 17:20:48 -0300

    Initial comit

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 import { PoMenuItem } from '@po-ui/ng-components';
 
@@ -10,15 +9,12 @@ import { PoMenuItem } from '@po-ui/ng-components';
 })
 export class AppComponent {
 
-  constructor(private router: Router){}
-
   readonly menus: Array<PoMenuItem> = [
-    { label: 'Paises', shortLabel: 'Paises', icon: 'po-icon-folder', action: this.onClick.bind(this, '/pais') },
-    { label: 'Pontos Turisticos', shortLabel: 'P. Turisticos', icon: 'po-icon-finance-secure', action: this.onClick.bind(this, 'ponto-turistico') }
+    { label: 'Home', action: this.onClick.bind(this) }
   ];
 
-  private onClick(rota: string) {
-    this.router.navigateByUrl(rota)
+  private onClick() {
+    alert('Clicked in menu item')
   }
 
 }
